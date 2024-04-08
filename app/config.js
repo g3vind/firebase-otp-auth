537c7e1d2a9da8c589dca62a6357c147ab0427a4
g3vind <g3vind@gmail.com>
2024-04-08 17:00:33 +0530

    added env

--- app/config.js
+++ app/config.js
@@ -1,17 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRV8A1jquNG_rtHA655OXD-lbX0CNGFik",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: "fir-auth-6444e.firebaseapp.com",
   projectId: "fir-auth-6444e",
   storageBucket: "fir-auth-6444e.appspot.com",
   messagingSenderId: "448873792522",
-  appId: "1:448873792522:web:ea66bcb96c550a6bddcbf3",
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
