3a613be8f7adecc8268bdb12e97ad93228489825
g3vind <g3vind@gmail.com>
2024-04-08 15:13:21 +0530

    minor changes

--- app/config.js
+++ app/config.js
@@ -1,4 +1,3 @@
-// -------------------
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import "firebase/auth"
@@ -7,12 +6,12 @@ import "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwf_KGcVgY9XjXAGc3AvdZpHzsKWSVGxw",
-  authDomain: "fir-auth-2f80e.firebaseapp.com",
-  projectId: "fir-auth-2f80e",
-  storageBucket: "fir-auth-2f80e.appspot.com",
-  messagingSenderId: "822505684078",
-  appId: "1:822505684078:web:edf0e780cd07e5f262fa7f",
+  apiKey: "AIzaSyCRV8A1jquNG_rtHA655OXD-lbX0CNGFik",
+  authDomain: "fir-auth-6444e.firebaseapp.com",
+  projectId: "fir-auth-6444e",
+  storageBucket: "fir-auth-6444e.appspot.com",
+  messagingSenderId: "448873792522",
+  appId: "1:448873792522:web:ea66bcb96c550a6bddcbf3",
 }
 
 // Initialize Firebase
